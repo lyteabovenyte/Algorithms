ffd1ccaafb13216514a01422513b63b19c051020
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-04 04:26:23 +0330

    added variadic join to built-in-join mock

--- Algorithms/Go/built-in-join-func/main.go
+++ Algorithms/Go/built-in-join-func/main.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	slice := []string{"apple", "banana", "mango", "amir!"}
 	output := join(",", slice)
-	fmt.Println(output)
+	fmt.Printf("normal join --> %v\n", output)
+
+	output2 := variadicJoin(",", "beach", "mountain", "jungle")
+	fmt.Printf("variadic join --> %v\n", output2)
 }
 
 func join(del string, values []string) string {
@@ -14,6 +17,17 @@ func join(del string, values []string) string {
 		line = line + ele
 		if i != len(values)-1 {
 			line = line + del + " "
+		}
+	}
+	return line
+}
+
+func variadicJoin(del string, values ...string) string {
+	var line string
+	for i, ele := range values {
+		line = line + ele
+		if i != len(values)-1 {
+			line += del + " "
 		}
 	}
 	return line
