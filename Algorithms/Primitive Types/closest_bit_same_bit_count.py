b850800adf6d7e2ed1f20a0d414d878118777706
lyteabovenyte <lyteabovenyte@gmail.com>
2024-12-29 18:47:17 +0330

    closest bit-same-weight

--- Algorithms/Primitive Types/closest_bit_same_bit_count.py
+++ Algorithms/Primitive Types/closest_bit_same_bit_count.py
@@ -1,3 +1,20 @@
 def closest_bit_same_bit_count(x):
-    lsb = x & 1
+    max_bits = 8   # assume the maximum number of bits of an integer is 8
+    for i in range (max_bits -1):
+        if (x >> i) & 1 != (x >> (i + 1)) & 1:
+            x ^= (1 >> i) | (1 >> i + 1)
+            return x
+    raise ValueError("All bits are 0 or 1.")
+
+print(bin(closest_bit_same_bit_count(0b1110101)))
+
+
+def weight(x):
+    result = 0
+    while x:
+        x &= x - 1
+        result += 1
+    return result
+
+print(weight(0b00101010100))
     
