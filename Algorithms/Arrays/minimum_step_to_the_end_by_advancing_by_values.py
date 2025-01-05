30bdf7063addeebcf38e40e563520fb7144fd1b4
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-05 16:10:53 +0330

    TODO

---
+++ Algorithms/Arrays/minimum_step_to_the_end_by_advancing_by_values.py
@@ -0,0 +1,9 @@
+'''
+    Advancing through an array: Given an array of integers that tells you the 
+    maximum distance you can advance forward from the given position, 
+    compute the minimum number of steps needed to reach the last location.
+    (You can advance by any value less than or equal to maximum distance.
+'''
+
+# TODO!
+def min_step_to_end(arr):
