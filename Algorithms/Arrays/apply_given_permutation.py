3e2a2eca0a58b0df8b90272f04c21a140d9ea8e3
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-07 14:00:14 +0330

    apply perm into array, cyclic approach

--- Algorithms/Arrays/apply_given_permutation.py
+++ Algorithms/Arrays/apply_given_permutation.py
@@ -14,11 +14,37 @@ def apply_permutation(arr, p):
 
     return arr
 
+# space complexity: O(n)
 def apply2(arr, p):
     res = [0] * len(p)
     for i in range(len(p)):
         res[p[i]] = arr[i]
     return res
 
-print(apply_permutation([3, 6, 5, 4], [1, 0, 3, 2]))
-print(apply2([3, 6, 5, 4], [1, 0, 3, 2]))
+
+# trying to solve it with space complexity of O(1)
+def apply_perm(perm, A):
+    for i in range(len(A)):
+        # check if the element at index i has not been moved
+        # by checking if perm[i] is not negative
+        next = i
+        while perm[next] >= 0:
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            temp = perm[next]
+            # subtracts len(perm) from an entry in perm
+            # to make it negative, which indicates the corresponding
+            # move has been performed
+            perm[next] -= len(perm)
+            next = temp
+    # Restore temp
+    perm[:] = [a + len(perm) for a in perm]
+    return A
+
+# the WOW approach
+def apply_wow(perm, A):
+    for i in range(len(A)):
+        while perm[i] != i:
+            A[perm[i]], A[i] = A[i], A[perm[i]]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
+    
+    return A
