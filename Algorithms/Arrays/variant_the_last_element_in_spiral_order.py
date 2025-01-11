e441f1be7e212ca1260c9367cdbeab2daae81cbf
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-11 04:59:54 +0330

    last element in spiral order

---
+++ Algorithms/Arrays/variant_the_last_element_in_spiral_order.py
@@ -0,0 +1,8 @@
+'''
+    Given two integer m & n, compute the last element in spiral order of an m X n 2D array
+'''
+
+def last_element(m, n):
+    return (m // 2, n // 2)
+
+print(last_element(3, 4))
