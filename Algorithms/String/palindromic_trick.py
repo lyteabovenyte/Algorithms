247df4b16d42ec086a805a35c0d4180412c40b56
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-12 04:10:19 +0330

    palindromic trick

--- Algorithms/String/palindromic_trick.py
+++ Algorithms/String/palindromic_trick.py
@@ -7,3 +7,13 @@ def palindromic(str):
     if str[i] != str[~i]: 
       return False
   return True
+
+def nicer_palindromic(s):
+  return all(s[i] == s[~i] for i in range(len(s) // 2))
+
+
+print(palindromic("amiralaeifar"))
+print(palindromic("amirima"))
+
+print(nicer_palindromic("amirima"))
+
