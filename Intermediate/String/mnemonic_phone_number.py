3b149c37132cec7335a176f3df87180540bfb08c
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-16 01:36:15 +0330

    added stepBystep recursive approach

--- Intermediate/String/mnemonic_phone_number.py
+++ Intermediate/String/mnemonic_phone_number.py
@@ -23,3 +23,23 @@ def mnemonic_phone_number(d):
     return [
         ''.join(mnemonic) for mnemonic in itertools.product(*(MAPPING[int(digit)] for digit in d))
     ]
+
+print(mnemonic_phone_number("22"))
+
+# recursive approach
+def recursive_phone_mnemonic(phone_number):
+    def recursive_func(digit):
+        if digit == len(phone_number):
+            # we have processed to the end, so join them and add to the result
+            result.append(''.join(partial_result))
+        else:
+            for c in MAPPING[int(phone_number[digit])]:
+                partial_result[digit] = c
+                recursive_func(digit + 1)
+    
+    result = []
+    partial_result = ['0'] * len(phone_number)
+    recursive_func(0)
+    return result
+
+print(recursive_phone_mnemonic("22"))
