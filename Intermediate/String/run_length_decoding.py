93ada45801334e826225b1653d1f998d116fc229
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-17 07:28:45 +0330

    two strategies for decoding in RL

--- Intermediate/String/run_length_decoding.py
+++ Intermediate/String/run_length_decoding.py
@@ -10,4 +10,19 @@ def run_length_decoding(s):
 
     return "".join(result)
 
+def better_run_length_decoding(s):
+    result = []
+    so_far = 0
+
+    for i in range(len(s)):
+        if s[i].isdigit():
+            so_far = so_far * 10 + int(s[i])
+        else:
+            result.append(s[i] * so_far)
+            so_far = 0
+
+    return "".join(result)
+    
+
 print(run_length_decoding("4a2b3c0l"))
+print(better_run_length_decoding("4a2b3c0l"))
