815862404a693a805256a30f3e8101268f2ddb9c
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-20 13:10:57 +0330

    another implementation of Insertion Sort

--- Intermediate/Sort/insertion_sort.py
+++ Intermediate/Sort/insertion_sort.py
@@ -1,19 +1,28 @@
 '''
 Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration.
-Insertion sort works similarly as we sort cards in our hand in a card game
-
 '''
-
 def insertion_sort(arr):
-    
     size = len(arr)
     i = 1
     while i < size:
         j = i
         temp = arr[i]
         while j > 0 and arr[j - 1] > temp:
-            arr[j] = arr[j - 1]
+            arr[j] = arr[j - 1] # swap
             j -= 1
         arr[j] = temp
         i += 1
     return arr
+
+# insert the integer n, into array A in a sorted position.
+def InsertionSort(A):
+    for i in range(1, len(A)):
+        j = i - 1
+        x = A[i]
+        while (j > -1 and A[j] > x):
+            A[j + 1] = A[j] # swap
+            j -= 1
+        A[j + 1] = x
+    return A
+
+print(InsertionSort([4, 2, 6, 3, 7]))
