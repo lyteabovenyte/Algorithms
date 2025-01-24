14af89842b4bb6949eed772c4a98c653ebcce7d2
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-24 23:42:40 +0330

    another implementation of reversing linkedlist

--- Intermediate/LinkedList/reverse_single_linkedlist.py
+++ Intermediate/LinkedList/reverse_single_linkedlist.py
@@ -2,6 +2,7 @@
     Reverse a singly linked list.
 '''
 from main import Node, LinkedList
+from ListNode import ListNode
 
 def reverse(L):
     current = L.head
@@ -15,7 +16,13 @@ def reverse(L):
     L.head = prev
     for ele in L:
         yield(ele)
-    
 
+# cooler implementation.
+def reverse_list(head):
+    dummy = ListNode(0)
+    while head:
+        dummy.next, head.next, head = head, dummy.next, head.next
+    return dummy.next
+    
 l = LinkedList(["amir", "lyteabovenyte", "lyteatnyte"])
 print(l.reverse())
