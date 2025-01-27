e8a0071a7abf6af4ef29aa8ea3a23351e6e0160e
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-27 03:59:17 +0330

    stacks APIs with bonus max API

--- Intermediate/Stacks and Queues/implement_a_stack_with_max_API.py
+++ Intermediate/Stacks and Queues/implement_a_stack_with_max_API.py
@@ -2,31 +2,26 @@ from collections import namedtuple
 
 class Stack:
     ElementsWithCachedMax = namedtuple('ElementsWithCachedMax', ('element', 'max'))
-    
     '''
         Design a stack that includes a max operatoins, in addition to push and pop. The 
         max method should return the maximum value stored in the stack. 
     '''
-    
     def __init__(self):
+        # internal underlying array.
         self._elements_with_cached_max = []
-        
         
     def is_empty(self):
         return len(self._elements_with_cached_max) == 0
-    
     
     def max(self):
         if self.is_empty():
             raise IndexError('max(): empty stack')
         return self._elements_with_cached_max[-1].max
     
-    
     def pop(self):
         if self.is_empty():
             raise IndexError('pop(): empty stack')
         return self._elements_with_cached_max.pop().element
-    
     
     def push(self, value):
         self._elements_with_cached_max.append(
