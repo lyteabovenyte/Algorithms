9a217c9a8c679c9b0f3e8bc8ce002f9db91fa2d0
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-28 20:55:31 +0330

    another implementation added

--- Intermediate/Arrays/minimum_step_to_the_end_by_advancing_by_values.py
+++ Intermediate/Arrays/minimum_step_to_the_end_by_advancing_by_values.py
@@ -4,6 +4,16 @@
     compute the minimum number of steps needed to reach the last location.
     (You can advance by any value less than or equal to maximum distance.
 '''
+from typing import List
 
-# TODO!
+# using greedy approach to get the minimum jumps to the end.
 def min_step_to_end(arr):
+    jump, current_end, farthest = 0, 0, 0
+    for i in range(len(arr) - 1):
+        farthest = max(farthest, arr[i] + i)
+        if i == current_end:
+            jump += 1
+            current_end = farthest
+    return jump
+
+print(min_step_to_end([2, 3, 1, 1, 4]))
