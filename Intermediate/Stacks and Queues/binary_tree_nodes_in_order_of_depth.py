245ee4df2d035f2a8e020066de0d20ba24216931
lyteabovenyte <lyteabovenyte@gmail.com>
2025-01-29 10:21:48 +0330

    binary tree nodes, based on depth

---
+++ Intermediate/Stacks and Queues/binary_tree_nodes_in_order_of_depth.py
@@ -0,0 +1,18 @@
+'''
+    Given a binary tree, return the nodes of each depth in order of depth started from the root.
+'''
+
+def binary_tree_depth_order(tree):
+    result = []
+    if not tree:
+        return result
+    
+    curr_depth_nodes = [tree]
+    while curr_depth_nodes:
+        result.append([curr.data for curr in curr_depth_nodes])
+        curr_depth_nodes = [
+            child
+            for curr in curr_depth_nodes for child in (curr.left, curr.right)
+            if child
+        ]
+    return result
