7736a7cc3f527d67ae788deca790b44e44be6072
lyteabovenyte <lyteabovenyte@gmail.com>
2025-02-04 20:31:10 +0330

    largest complete subtree. Worth Noting

--- Intermediate/Binary_Trees/variant_size_of_largest_complete_subtree.py
+++ Intermediate/Binary_Trees/variant_size_of_largest_complete_subtree.py
@@ -1,27 +1,32 @@
 """
-    Write a program which returns the size of the largest subtree.
+    Write a program which returns the size of the largest complete subtree.
+    A complete binary tree is a binary tree in which:
+    - Every level, except possibly the last, is completely filled.
+    - All nodes in the last level are as left as possible.
+
 """
+import collections
+
 def largest_subtree(tree):
-    if not tree:
-        return 0
-    max_size_so_far = 0
+    CompleteSubtreeWithSize = collections.namedtuple("CompleteSubtreeWithSize", (
+        "size", "height", "is_complete"
+    ))
+    def dfs(root):
+        if not root:
+            return CompleteSubtreeWithSize(0, 0, True)
 
-    def size(tree):
-        if not tree:
-            return 0
-        return size(tree.left) + size(tree.right) + 1
+        ls, lh, lc = dfs(root.left)
+        rs, rh, rc = dfs(root.right)
 
-    def is_complete(tree):
-        if tree.left and tree.right:
-            return True
-        return False
+        # check if the current subtree is complete
+        if lc and rc and (lh == rh or lh == rh + 1):
+            size = ls + rs + 1
+            height = max(lh, rh) + 1
+            max_so_far[0] = max(max_so_far[0], size)
+            return CompleteSubtreeWithSize(size, height, True)
+        return CompleteSubtreeWithSize(0, 0, False)
 
-    left_subtree = is_complete(tree.left)
-    if left_subtree:
-        max_size_so_far = max(size(tree.left), max_size_so_far)
+    max_so_far = [0]
+    dfs(tree)
+    return max_so_far[0]
 
-    right_subtree = is_complete(tree.right)
-    if right_subtree:
-        max_size_so_far = max(size(tree.right))
-
-    return max_size_so_far
