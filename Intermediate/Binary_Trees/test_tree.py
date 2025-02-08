d38a4988f56fcb27a387a065ad13d6bb00e4e456
lyteabovenyte <lyteabovenyte@gmail.com>
2025-02-08 06:54:44 +0330

    ignore.

---
+++ Intermediate/Binary_Trees/test_tree.py
@@ -0,0 +1,12 @@
+from BinaryTreeNode import BinaryTreeNode as N
+from BinaryTreeUtils import (binary_tree_to_string, generate_preorder,
+                                generate_inorder)
+
+
+tree = N(1, N(2, N(3, N(4, None, None),
+                   None), N(5, None, None)),
+         N(6, N(7, None, N(8, None, None))))
+
+print(binary_tree_to_string(tree))
+
+
