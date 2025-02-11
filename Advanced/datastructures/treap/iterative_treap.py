4cc24e0c908d21872d031e240a4ca818576e6f19
lyteabovenyte <lyteabovenyte@gmail.com>
2025-02-11 06:28:42 +0330

    tuning TreapNode and added more funtionality to the treap

--- Advanced/datastructures/treap/iterative_treap.py
+++ Advanced/datastructures/treap/iterative_treap.py
@@ -1,6 +1,3 @@
-"""
-    Testing my treaps knowledge on implementing a Treap class and all of it's API methods
-"""
 import random
 
 class TreapNode:
@@ -13,43 +10,36 @@ class TreapNode:
 
     def set_left(self, node):
         self.left = node
-        if node != None:
+        if node is not None:
             node.parent = self
 
     def set_right(self, node):
         self.right = node
-        if node != None:
+        if node is not None:
             node.parent = self
 
 
-
-
 class Treap:
-
     def __init__(self):
         self.root = None
 
     # ---- Utility Functions -----
-
     def _is_root(self, node):
-        if node.parent == None:
-            return True
-        return False
+        return node.parent is None
+    
+    def _is_leaf(self, node):
+        return not node.left and not node.right
 
     # ---- Rotations -----
-    """
-        Note how we manage the parent field
-        in each rotation.
-    """
     def _rotate_right(self, node):
         """ right rotation for balancing"""
         if node is None or self._is_root(node):
             raise ValueError(f'Cannot rotate {node.key}. `Root` does not have `Parent` field.')
         y = node.parent
-        assert y.left != node  # Right rotation can only be applied to left childrens
+        assert y.left == node  # Right rotation can only be applied to left children
         p = y.parent
 
-        if p != None:
+        if p is not None:
             if p.left == y:
                 p.set_left(node)
             else:
@@ -67,7 +57,7 @@ class Treap:
         assert y.right == node
         p = y.parent
 
-        if p != None:
+        if p is not None:
             if p.left == y:
                 p.set_left(node)
             else:
@@ -78,16 +68,7 @@ class Treap:
         y.set_right(node.left)
         node.set_left(y)
 
-    
     # ------ Insert Operation ------
-    """
-        Insert operation comes in two steps:
-        1. Insert at the leaf (like Binary tree)
-        we keep track of the parent node
-        to be able to add the new_node as the child of leaf.
-        2. check heap invariants, if violated, 
-        rotate the subtree rooted at the parent node
-    """
     def _insert(self, key, priority):
         node, parent = self.root, None
         new_node = TreapNode(key, priority)
@@ -96,7 +77,7 @@ class Treap:
         while node is not None: 
             # at the end of this loop, parent would point at a leaf.
             parent = node
-            if node.key <= key:
+            if key < node.key:
                 node = node.left
             else:
                 node = node.right
@@ -104,46 +85,71 @@ class Treap:
         if parent is None:
             self.root = new_node
             return
-        elif key <= parent.key:
-            parent.left = new_node
+        elif key < parent.key:
+            parent.set_left(new_node)
         else:
-            parent.right = new_node
+            parent.set_right(new_node)
 
-        new_node.parent = parent
         # then check for heap invariants
-        while new_node.parent != None:
-            if new_node.priority < new_node.parent.priority: # Min-Heap
-                if new_node == new_node.parent.left:
-                    self._rotate_right(new_node)
-                else:
-                    self._rotate_left(new_node)
-
-        if new_node.parent == None:
-            # in this case we need to update
-            # our Treap's root value to new_node
-            self.root = new_node
-        
-
-        # ----- Search Operation -----
-
-        def _search(self, node, target): # `node` is the starting point (root) for search
-            if node== None:
-                return None
-            if node.key == target:
-                return node
-            elif target < node.key:
-                return self._search(node.left, target)
+        while new_node.parent is not None and new_node.priority > new_node.parent.priority: 
+            if new_node == new_node.parent.left:
+                self._rotate_right(new_node)
             else:
-                return self._search(node.right, target)
+                self._rotate_left(new_node)
 
+        if new_node.parent is None:
+            self.root = new_node
 
-        # ------- Delete Operation -------
-        """
-            with respect to BST and it's delete operation
-            it is conceptually different.
-            we assign the least priority to the node, and push it down
-            until it reaches the leaf, so we easily remove it.
-        """
-        def _delete(self, key) -> bool:
-            node = self._search(self.root, key)
+    def insert(self, key):
+        self._insert(key, random.randint(0, 10**9))
+
+    # ----- Search Operation -----
+    def _search(self, node, target):
+        if node is None:
+            return None
+        if node.key == target:
+            return node
+        elif target < node.key:
+            return self._search(node.left, target)
+        else:
+            return self._search(node.right, target)
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    # ------- Delete Operation -------
+    def _delete(self, key) -> bool:
+        node = self._search(self.root, key)
+        if node is None:
+            return False
+        if self._is_root(node) and self._is_leaf(node):
+            self.root = None
+            return True
+        
+        while not self._is_leaf(node):
+            if node.left and (node.right is None or node.right.priority < node.left.priority):
+                    self._rotate_right(node.left)
+            else:
+                self._rotate_left(node.right)
+
+            if self._is_root(node.parent):
+                self.root = node.parent
             
+        # at this point `node` is leaf and we disconnect it from its parent
+        if node.parent.left == node:
+            node.parent.left = None
+        else:
+            node.parent.right = None
+        return True
+
+    def delete(self, key):
+        return self._delete(key)
+    
+    def top(self):
+        if self.root is None:
+            raise ValueError('Empty Treap.')
+        key = self.root.key
+        self._delete(key)
+        return key
+    
+    
