8aa3a28f8cc5f202fef97854caa48505e303b1ce
lyteabovenyte <lyteabovenyte@gmail.com>
2025-03-21 20:11:05 +0330

    getting result with hugging face pretrained gpt2

--- Neural_networks/Andrej_Karpathy/GPT/train_gpt2.py
+++ Neural_networks/Andrej_Karpathy/GPT/train_gpt2.py
@@ -168,6 +168,49 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
-    
+# -----------------------------------------------------------------------------------------------------------------------------
+# attemp to autodetect the device
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    devidce = "mps"
+print(f"using device: {device}")
+
+num_return_sequences = 5
+max_length = 30
+
 model = GPT.from_pretrained('gpt2')
-print('done')
+model.eval()
+model.to(device)
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to(device)
+
+
+# generate! right now x is (B,T) where B = 5 and T = 8
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    # forward the model to get the logits
+    with torch.no_grad():
+        logits = model(x)
+        logits = logits[0]
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+# -----------------------------------------------------------------------------------------------------------------------------
